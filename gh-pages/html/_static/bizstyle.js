53974962f15d2205354d9a8e1de7870d965bc1ff
Reint <33000433+reint-fischer@users.noreply.github.com>
2021-02-09 08:52:37 +0100

    Update index.rst to follow new structure

--- gh-pages/html/_static/bizstyle.js
+++ gh-pages/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Parcels  documentation");
+        $("li.nav-item-0 a").text("Parcels 2.2.1 documentation");
     }
 });
