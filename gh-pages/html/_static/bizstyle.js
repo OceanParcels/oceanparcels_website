2c633ed95b99677edd4d5a3e7d91b7a7890c7d0d
Erik van Sebille <erikvansebille@gmail.com>
2022-10-06 17:08:31 +0200

    Updating documentation to v2.4.0

--- gh-pages/html/_static/bizstyle.js
+++ gh-pages/html/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Parcels 2.3.1 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Parcels 2.4.0 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
