5e8193aeb2004b37342e9eba5f6ac81f1fd39a19
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-03-25 18:44:37 +0100

    Remove twitter meta

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,7 +43,6 @@ export const Layout = ({
         <meta name='twitter:description' content={description} />
         <meta name='twitter:image' content={fullCardUrl} />
         <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@xarray_dev' />
         <link
           rel='icon'
           type='image/png'
