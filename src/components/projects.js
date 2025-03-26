fd546fa7aa3bf6324588f05f1459d7f87e032f57
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-03-26 14:50:51 +0100

    Remove dead code

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,50 +1,9 @@
-import {
-  Box,
-  Button,
-  Container,
-  Stack,
-  SimpleGrid,
-  Text,
-} from '@chakra-ui/react'
+import { Box, Container, SimpleGrid, Text } from '@chakra-ui/react'
 import React from 'react'
 
-import { Heading, Link } from '@/components/mdx'
+import { Heading } from '@/components/mdx'
 import { ProjectCard } from '@/components/project-card'
 import { Projects as ProjectsData } from '@/data/projects'
-
-import { IoLogoGithub } from 'react-icons/io5'
-
-import useSWR from 'swr'
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
-
-const GitHubStats = () => {
-  const { data, error } = useSWR(
-    'https://xarray-datasette.fly.dev/github/_analyze_tables_/stars,user.json?_shape=array',
-    fetcher,
-  )
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-
-  return (
-    <Stack direction='row' spacing={4} justify='center'>
-      <Button
-        as={Link}
-        href={'https://github.com/pydata/xarray/stargazers'}
-        rounded={'full'}
-        size={'lg'}
-        leftIcon={<IoLogoGithub />}
-        variant='outline'
-      >
-        {data[0].total_rows.toLocaleString(undefined, {
-          minimumFractionDigits: 0,
-        })}{' '}
-        Stars
-      </Button>
-    </Stack>
-  )
-}
 
 export const Projects = () => {
   const projects = React.useMemo(() => ProjectsData, [])
