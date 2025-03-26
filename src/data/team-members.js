f2094fb7d1a7ba4d26f6dcf3b3bd4c446b1eeb10
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-03-26 13:27:53 +0100

    update team members data

--- src/data/team-members.js
+++ src/data/team-members.js
@@ -1,27 +1,82 @@
 export const TeamMembers = [
-  // TODO: Update this data
-  { name: 'Stephan Hoyer', github: 'shoyer' },
-  { name: 'Joe Hamman', github: 'jhamman' },
-  { name: 'Ryan Abernathey', github: 'rabernat' },
-  { name: 'Benôit Bovy', github: 'benbovy' },
-  { name: 'Fabien Maussion', github: 'fmaussion' },
-  { name: 'Keisuke Fujii', github: 'fujiisoup' },
-  { name: 'Maximilian Roos', github: 'max-sixty' },
-  { name: 'Deepak Cherian', github: 'dcherian' },
-  { name: 'Spencer Clark', github: 'spencerkclark' },
-  { name: 'Tom Nicholas', github: 'TomNicholas' },
-  { name: 'Guido Imperiale', github: 'crusaderky' },
-  { name: 'Justus Magin', github: 'keewis' },
-  { name: 'Mathias Hauser', github: 'mathause' },
-  { name: 'Anderson Banihirwe', github: 'andersy005' },
-  { name: 'Jimmy Westling', github: 'Illviljan' },
-  { name: 'Alessandro Amici', github: 'alexamici' },
-  { name: 'Aureliana Barghini', github: 'aurghs' },
-  { name: 'Mick Niklas', github: 'headtr1ck' },
-  { name: 'Kai Mühlbauer', github: 'kmuehlbauer' },
-  { name: 'Scott Henderson', github: 'scottyhq' },
-  { name: 'Jessica Scheick', github: 'JessicaS11' },
-  { name: 'Matt Savoie', github: 'flamingbear' },
-  { name: 'Owen Littlejohns', github: 'owenlittlejohns' },
-  { name: 'Eni Awowale', github: 'eni-awowale' },
+  {
+    name: 'Vesna Bertoncelj',
+    position: 'PhD candidate',
+    description:
+      'Vesna investigates how pollutants spread around coral reefs on Curacao.',
+  },
+  {
+    name: 'Meike Bos',
+    position: 'Postdoctoral researcher',
+    description:
+      'Meike develops parameterisations for the dynamics of macroplastic items in oceanic flows.',
+  },
+  {
+    name: 'Emma Daniëls',
+    position: 'Postdoctoral researcher',
+    description: 'Emma develops a virtual ship learning experience.',
+  },
+  {
+    name: 'Michael Denes',
+    position: 'Postdoctoral researcher',
+    description:
+      'Michael investigates how marine pollution can be computed in the Copernicus Marine Service.',
+    links: {
+      github: 'https://github.com/michaeldenes',
+      'research-gate': 'https://www.researchgate.net/profile/Michael-Denes',
+      'google-scholar':
+        'https://scholar.google.com/citations?user=KIx5ROoAAAAJ',
+    },
+  },
+  {
+    name: 'Nick Hodgskin',
+    position: 'Research software engineer',
+    description: 'Nick supports the development and use of the parcels code.',
+    links: {
+      github: 'https://github.com/veckothegecko/',
+    },
+  },
+  {
+    name: 'Laura Gomez Navarro',
+    position: 'Margarita Salas Fellow',
+    description:
+      'Laura investigates how oceanic fine-scales affect the transport of marine plastic.',
+    links: {
+      github: 'https://github.com/LauraGomezNavarro',
+      'research-gate':
+        'https://www.researchgate.net/profile/Laura-Gomez-Navarro',
+      'google-scholar':
+        'https://scholar.google.es/citations?user=ML8EsI4AAAAJ&hl=ca',
+    },
+  },
+  {
+    name: 'Jimena Medina Rubio',
+    position: 'PhD candidate',
+    description:
+      'Jimena investigates how ocean currents transport plastic in the North Sea.',
+  },
+  {
+    name: 'Isabelle Pelster José',
+    position: 'BSc student',
+    description:
+      'Isabelle analyses how drifters track Sargassum in the Tropical Atlantic Ocean.',
+  },
+  {
+    name: 'Marc Schneiter',
+    position: 'PhD candidate',
+    description:
+      'Marc develops new drifter tracking technology to investigate beaching of large plastic items.',
+  },
+  {
+    name: 'Erik van Sebille',
+    position: 'Professor',
+    description:
+      "Erik investigates how ocean currents move 'stuff' around, and leads the OceanParcels project.",
+    links: {
+      github: 'https://github.com/erikvansebille',
+      'google-scholar':
+        'https://scholar.google.com.au/citations?user=2Pb1HdUAAAAJ&hl=en',
+      'UU profile': 'https://www.uu.nl/staff/EvanSebille',
+    },
+  },
 ]
