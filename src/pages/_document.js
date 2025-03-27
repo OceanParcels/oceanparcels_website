bb38029e0e9311168dcddde1e50325ce365446e1
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-03-27 14:43:14 +0100

    Fix head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
+        <Head />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
