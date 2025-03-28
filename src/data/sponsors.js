543c881b5a9698952125dd3472f7f5fb3b2c0840
Erik van Sebille <erikvansebille@gmail.com>
2025-03-28 14:45:12 +0100

    Updating sponsors list and adding logos

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,12 +1,22 @@
 export const Sponsors = [
   {
-    name: 'Parcels',
-    logo: '/parcels-assets/logo-horo.svg',
-    url: 'https://www.example.com/',
+    name: 'The European Research Council under the H2020 Starting Grant TOPIOS project (grant agreement No 715386).',
+    logo: '/sponsors-logos/LOGO_ERC-FLAG_FP.png',
+    url: 'http://topios.org/',
   },
   {
-    name: 'Parcels',
-    logo: '/parcels-assets/logo-horo.svg',
-    url: 'https://www.example.com/',
-  }, // TODO: Update
+    name: 'The Dutch Research Council (NWO)',
+    logo: '/sponsors-logos/NWO logo - full colour - RGB - transparent background.png',
+    url: 'https://www.nwo.nl/en',
+  },
+  {
+    name: 'Utrecht University',
+    logo: '/sponsors-logos/UU_logo_2021_EN_RGB.png',
+    url: 'https://www.uu.nl/en',
+  },
+  {
+    name: 'ESPRC',
+    logo: '/sponsors-logos/UKRI_EPSR_Council-Logo_Horiz-RGB.png',
+    url: 'https://www.ukri.org/councils/epsrc/',
+  },
 ]
