bb23b9b872063e1b574c711df23904562ff85eb0
Erik van Sebille <erikvansebille@gmail.com>
2025-03-31 10:30:08 +0200

    Updates on Features texts

--- src/data/features.js
+++ src/data/features.js
@@ -1,6 +1,6 @@
 export const Features = [
   {
-    title: 'Use data from any rectilinear or curvilinear horizontal grid',
+    title: 'Use data from any (curvilinear) grid',
     text: 'Parcels works on model outputs including NEMO, POP, MITgcm, and more.',
   },
   {
@@ -9,23 +9,23 @@ export const Features = [
   },
   {
     title: 'Define and execute custom kernels',
-    text: 'Encode custom advection, diffusion, and other particle operations.',
+    text: 'Encode custom particle operations such as swimming or sinking.',
   },
   {
-    title: 'Advection of particles in 2D and 3D',
-    text: 'Using built-in kernels for Runge-Kutta4 and Runge-Kutta45.',
+    title: 'Advect particles in 2D and 3D',
+    text: 'Use built-in kernels for Runge-Kutta4 and Runge-Kutta45 schemes.',
   },
   {
-    title: 'Horizontal diffusion of particles',
-    text: 'Through built-in kernels using the Milstein and Euler-Maruyama schemes.',
+    title: 'Diffuse particles in 2D',
+    text: 'Use built-in kernels for the Milstein and Euler-Maruyama schemes.',
   },
   {
-    title: 'Start and remove Particles during execution',
+    title: 'Start and stop particles during execution',
     text: 'For example, to simulate oil spills or fish spawning.',
   },
   {
-    title: 'Particle-particle interactions',
-    text: 'Simulate particle-particle interactions, such as aggregation and mortality.',
+    title: 'Simulate particle-particle interactions',
+    text: 'For example, to simulate aggregation or mortality.',
   },
   {
     title: 'Output particle data in zarr format',
